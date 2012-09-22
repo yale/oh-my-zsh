d958b3095a0b9138447ad870189fc0b6ecc21e61
Yale Spector <yspector@mdsol.com>
2012-09-22 13:13:03 -0400

    don't alias mysql

--- lib/correction.zsh
+++ lib/correction.zsh
@@ -2,7 +2,7 @@ setopt correct_all
 
 alias man='nocorrect man'
 alias mv='nocorrect mv'
-alias mysql='nocorrect mysql'
+#alias mysql='nocorrect mysql'
 alias mkdir='nocorrect mkdir'
 alias gist='nocorrect gist'
 alias heroku='nocorrect heroku'
